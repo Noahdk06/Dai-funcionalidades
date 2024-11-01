3184d2c16945bc65156e605f91c797f59273f2fd
Android Studio <devandroid@ort>
2024-11-01 11:09:00 -0300

    domamos al pelado

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 import QRCode from 'react-native-qrcode-svg';
 
 const AboutScreen = () => {
-  const qrValue = "Integrantes: Juan, María, Pedro";
+  const qrValue = "Integrantes: Iván Frankowski, Noah Denenberg Korob y Uma Gotfryd";
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
